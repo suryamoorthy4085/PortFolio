9622ec0448684167216d572d190e2d343b768573
Surya Moorthy K <113605360+suryamoorthy4085@users.noreply.github.com>
2024-02-27 09:27:40 +0530

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,8 @@
+const typed = new Typed('.typing-text',{
+	strings: ['a Fresher', 'a 2023 Passed out', 'Graduated in B.E. Computer Science and Engineering'],
+	typeSpeed: 70,
+	backSpeed: 70,
+	backDelay: 1000,
+	loop: true,
+});
+
