c0de930911c8608634a371340b71927012ef8edd
Surya Moorthy K <113605360+suryamoorthy4085@users.noreply.github.com>
2024-03-16 18:03:02 +0530

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -1,8 +1,1 @@
-const typed = new Typed('.typing-text',{
-	strings: ['a Fresher', 'a 2023 Passed out', 'Graduated in B.E. Computer Science and Engineering'],
-	typeSpeed: 70,
-	backSpeed: 70,
-	backDelay: 1000,
-	loop: true,
-});
 
